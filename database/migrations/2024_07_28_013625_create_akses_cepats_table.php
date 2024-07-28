d4e80c8f0fa0236ab84889f48376df8620a2233c
Giafn <giafauzan11@gmail.com>
2024-07-28 09:01:57 +0700

    update migration

---
+++ database/migrations/2024_07_28_013625_create_akses_cepats_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('akses_cepats', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('nama');
+            $table->enum('type', ['page', 'url']);
+            $table->string('url');
+            $table->foreignUuid('page_id')->nullable()->constrained('pages', 'id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('akses_cepats');
+    }
+};
---
+++ database/migrations/2024_07_28_013738_create_layanan_informasi_lists_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('layanan_informasi_lists', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('nama');
+            $table->enum('type', ['page', 'url']);
+            $table->string('url');
+            $table->foreignUuid('page_id')->constrained('pages', 'id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('layanan_informasi_lists');
+    }
+};
