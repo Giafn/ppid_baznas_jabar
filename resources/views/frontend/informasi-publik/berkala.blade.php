226ea8ba229074e741697c364991a6a6afc543be
Giafn <giafauzan11@gmail.com>
2024-08-11 21:21:43 +0700

    update tampilan tombol

--- resources/views/frontend/informasi-publik/berkala.blade.php
+++ resources/views/frontend/informasi-publik/berkala.blade.php
@@ -11,18 +11,20 @@
                 @forelse ($groupedItems as $key => $items)
                     <div class="col-12 mb-4">
                         <h4 class="text-green-primary">{{ $key }}</h4>
-                        <div class="list-group">
+                        <div class="row">
                             @foreach ($items as $item)
-                                <a href="{{ $item->url }}" class="list-group-item list-group-item-action fs-5">
-                                    <div class="d-flex justify-content-between">
-                                        <div class="flex-grow-1">
-                                            {{ $item->nama }}
+                                <div class="col-md-6">
+                                    <a href="{{ $item->url }}" class="btn bg-green-primary fs-5 d-block">
+                                        <div class="d-flex justify-content-between">
+                                            <div class="flex-grow-1">
+                                                {{ $item->nama }}
+                                            </div>
+                                            <div>
+                                                <i class="fas fa-external-link-alt"></i>
+                                            </div>
                                         </div>
-                                        <div>
-                                            <i class="fas fa-external-link-alt"></i>
-                                        </div>
-                                    </div>
-                                </a>
+                                    </a>
+                                </div>
                             @endforeach
                         </div>
                     </div>
