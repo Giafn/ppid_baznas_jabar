b48a8a31ae5b9c284f72fd46c4d08b8adc65e8ef
Giafn <giafauzan11@gmail.com>
2024-08-11 14:50:25 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call(
             [
                 GeneralDataSeeder::class,
+                SettingSeeder::class,
             ]
         );
     }
