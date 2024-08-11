28894e7a1bf683785faec0dd45e5a059f6c7cd85
Giafn <giafauzan11@gmail.com>
2024-08-11 20:53:56 +0700

    update

--- app/Http/Controllers/ItemsRequiredPerpageController.php
+++ app/Http/Controllers/ItemsRequiredPerpageController.php
@@ -14,9 +14,9 @@ class ItemsRequiredPerpageController extends Controller
     public function get()
     {
         $formulir = Formulir::select('id', 'nama')->orderBy('created_at', 'asc')->get();
-        $itemsLayanan = LayananInformasiList::select('id', 'nama', 'url')->orderBy('created_at', 'asc')->get();'
-        $regulasi = Regulasi::select('id', 'nama', 'url')->orderBy('created_at', 'asc')->get();'
-        $laporan = LaporanList::select('id', 'nama', 'url')->orderBy('created_at', 'asc')->get();'
+        $itemsLayanan = LayananInformasiList::select('id', 'nama', 'url')->orderBy('created_at', 'asc')->get();
+        $regulasi = Regulasi::select('id', 'nama', 'url')->orderBy('created_at', 'asc')->get();
+        $laporan = LaporanList::select('id', 'nama', 'url')->orderBy('created_at', 'asc')->get();
         $settings = Setting::where('key', 'informasi_kantor')->first();
         $infoKantor = json_decode($settings->value);
         $embedMap = Setting::where('key', 'embed_map')->first()->value;
